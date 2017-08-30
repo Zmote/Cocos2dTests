c2a4a83069150e73cda350b04e74758f11033455
DESKTOP-3M9COAN\Zmote <shodaime.hikarikage@gmail.com>
2017-08-30 06:46:22 +0200

    base setup for race track and movement

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -9,12 +9,9 @@ using namespace cocostudio::timeline;
 Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
-    auto scene = Scene::createWithPhysics();
-	//scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
-	//scene->getPhysicsWorld()->setGravity(Vec2(0, 0));
+    auto scene = Scene::create();
     // 'layer' is an autorelease object
 	auto layer = HelloWorld::create();
-	layer->setPhysicsWorld(scene->getPhysicsWorld());
 
     // add layer as a child to scene
     scene->addChild(layer);
@@ -32,63 +29,76 @@ bool HelloWorld::init()
     
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-	auto edgeBody = PhysicsBody::createEdgeBox(visibleSize, PHYSICSBODY_MATERIAL_DEFAULT, 3);
 
-	auto edgeNode = Node::create();
-	edgeNode->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y);
-	edgeNode->setPhysicsBody(edgeBody);
-	addChild(edgeNode);
+	background = experimental::TMXTiledMap::create("racetrack_map.tmx");
+	background->setScale(2, 2);
 
+	addChild(background);
+	
 	sprite1 = Sprite::create("zafer.jpg");
 	sprite1->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y);
-	sprite1->setScale(0.5, 0.5);
-	auto sprite1Body = PhysicsBody::createBox(sprite1->getContentSize(), PhysicsMaterial(0, 1, 0));
-	sprite1->setPhysicsBody(sprite1Body);
-	sprite1->getPhysicsBody()->setCollisionBitmask(1);
-	sprite1->getPhysicsBody()->setContactTestBitmask(true);
-
-	sprite2 = Sprite::create("zafer.jpg");
-	sprite2->setPosition(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y + 100);
-	sprite2->setScale(0.5, 0.5);
-	auto sprite2Body = PhysicsBody::createBox(sprite2->getContentSize(), PhysicsMaterial(0, 1, 0));
-	sprite2->setPhysicsBody(sprite2Body);
-	sprite2->getPhysicsBody()->setCollisionBitmask(2);
-	sprite2->getPhysicsBody()->setContactTestBitmask(true);
+	sprite1->setScale(0.2, 0.2);
 
 	addChild(sprite1);
-	addChild(sprite2);
 
 	auto eventListener = EventListenerKeyboard::create();
 	auto eventReleaseListener = EventListenerKeyboard::create();
 	eventReleaseListener->onKeyReleased = CC_CALLBACK_2(HelloWorld::keyReleased, this);
 	eventListener->onKeyPressed = CC_CALLBACK_2(HelloWorld::keyPressed, this);
 
-	auto contactListener = EventListenerPhysicsContact::create();
-	contactListener->onContactBegin = CC_CALLBACK_1(HelloWorld::onContactBegin, this);
-
-	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(contactListener, this);
 	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(eventListener, this);
 	this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(eventReleaseListener, this);
+
 	this->scheduleUpdate();
     return true;
 }
 
 void HelloWorld::update(float delta)
 {
-	if (arrow_states[LEFT]) sprite1->getPhysicsBody()->applyForce(Vec2(-10,0));
-	if (arrow_states[TOP]) sprite1->getPhysicsBody()->applyForce(Vec2(0, 10));
-	if (arrow_states[RIGHT]) sprite1->getPhysicsBody()->applyForce(Vec2(10, 0));
-	if (arrow_states[DOWN]) sprite1->getPhysicsBody()->applyForce(Vec2(0, -10));
-}
+	//BOUNDARIES without using physics, map movement as character movement
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	bool canMove[4] = {true,true,true,true};
 
-bool HelloWorld::onContactBegin(PhysicsContact& pContact)
-{
-	PhysicsBody * a = pContact.getShapeA()->getBody();
-	PhysicsBody * b = pContact.getShapeB()->getBody();
-	if ((1 == a->getCollisionBitmask() && 2 == b->getCollisionBitmask()) || (2 == a->getCollisionBitmask() && 1 == b->getCollisionBitmask())) {
-		log("COLLISION DETECTED");
+	if(!background->getBoundingBox().containsPoint(sprite1->getPosition()))
+	{
+		if(background->getPositionX() > visibleSize.width/2)
+		{
+			log("REACHED LEFT LIMIT");
+			canMove[LEFT] = false;
+		}
+		if(background->getPositionX() <= -visibleSize.width)
+		{
+			log("REACHED RIGHT LIMIT");
+			canMove[RIGHT] = false;
+		}
+		if(background->getPositionY() > visibleSize.height/2)
+		{
+			log("REACHED DOWN LIMIT");
+			canMove[DOWN] = false;
+		}
+		if(background->getPositionY() <= -visibleSize.height)
+		{
+			log("REACHED TOP LIMIT");
+			canMove[TOP] = false;
+		}
 	}
-	return true;
+
+	if (arrow_states[LEFT] && canMove[LEFT])
+	{
+		background->runAction(MoveBy::create(0, Vec2(10, 0)));
+	}
+	if (arrow_states[RIGHT] && canMove[RIGHT])
+	{
+		background->runAction(MoveBy::create(0, Vec2(-10, 0)));
+	}
+	if (arrow_states[TOP] && canMove[TOP])
+	{
+		background->runAction(MoveBy::create(0, Vec2(0, -10)));
+	}
+	if (arrow_states[DOWN] && canMove[DOWN])
+	{
+		background->runAction(MoveBy::create(0, Vec2(0, 10)));
+	}
 }
 
 void HelloWorld::keyPressed(EventKeyboard::KeyCode keyCode, Event* event)
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -8,18 +8,17 @@ class HelloWorld : public cocos2d::Layer
 public:
 	Node * rootNodePtr;
 	Node * sprite1;
-	Node * sprite2;
+	cocos2d::experimental::TMXTiledMap * background;
 	enum ARROWS{LEFT, TOP, RIGHT, DOWN};
 	bool arrow_states[4];
 	// there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-	bool onContactBegin(cocos2d::PhysicsContact& pContact);
 	void keyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event * event);
 	void keyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event * event);
 
 	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-	virtual void update(float delta) override;
+	void update(float delta) override;
 private:
 	cocos2d::PhysicsWorld* sceneWorld;
 	void setPhysicsWorld(cocos2d::PhysicsWorld * world);
