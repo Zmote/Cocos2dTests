d697a2132dbf117d96d66a271771a66258559132
DESKTOP-3M9COAN\Zmote <shodaime.hikarikage@gmail.com>
2017-08-31 12:25:09 +0200

    added finish, added track borders, little refactoring here and there

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,8 +1,7 @@
 #include "HelloWorldScene.h"
 #include "cocostudio/CocoStudio.h"
 #include "ui/CocosGUI.h"
-#include <winuser.h>
-#include <winuser.h>
+#include <memory>
 
 USING_NS_CC;
 
@@ -11,7 +10,9 @@ using namespace cocostudio::timeline;
 Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
-    auto scene = Scene::create();
+    auto scene = Scene::createWithPhysics();
+	scene->getPhysicsWorld()->setGravity(Vec2(0, 0));
+	//scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
     // 'layer' is an autorelease object
 	auto layer = HelloWorld::create();
 
@@ -28,9 +29,8 @@ bool HelloWorld::init()
     {
         return false;
     }
-    
-	Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
+	    
 	background = experimental::TMXTiledMap::create("racetrack_map.tmx");
 	background->setScale(2, 2);
 	
@@ -45,6 +45,8 @@ bool HelloWorld::init()
 	auto width = spawnPoint["width"].asFloat();
 	sprite1->setPosition(Vec2(xPos + width, yPos + height));
 	sprite1->setScale(0.2f, 0.2f);
+	auto sprite1Body = PhysicsBody::createBox(sprite1->getContentSize());
+	sprite1->setPhysicsBody(sprite1Body);
 	
 	addChild(sprite1);
 
@@ -58,6 +60,18 @@ bool HelloWorld::init()
 	auto finishLineObject = background->getObjectGroup("Objects")->getObject("finishLine");
 	finishLineRectangle = Rect{ 2 * finishLineObject["x"].asFloat(), 2 * finishLineObject["y"].asFloat(),
 		2 * finishLineObject["width"].asFloat(),2 * finishLineObject["height"].asFloat() };
+
+	auto leftBorder = background->getObjectGroup("Objects")->getObject("leftTerrain");
+	auto leftSprite = Sprite::create();
+	leftSprite->setPhysicsBody(PhysicsBody::createBox(Size(leftBorder["width"].asFloat(), leftBorder["height"].asFloat())));
+	leftSprite->setPosition(Size(leftBorder["width"].asFloat()/2, leftBorder["height"].asFloat()/2));
+	background->addChild(leftSprite);
+
+	auto rightBorder = background->getObjectGroup("Objects")->getObject("rightTerrain");
+	auto rightSprite = Sprite::create();
+	rightSprite->setPhysicsBody(PhysicsBody::createBox(Size(rightBorder["width"].asFloat(), rightBorder["height"].asFloat())));
+	rightSprite->setPosition(Size(rightBorder["x"].asFloat() + rightBorder["width"].asFloat()/2 , rightBorder["y"].asFloat() + rightBorder["height"].asFloat()/2));
+	background->addChild(rightSprite);
 
 	auto eventListener = EventListenerKeyboard::create();
 	auto eventReleaseListener = EventListenerKeyboard::create();
@@ -74,51 +88,10 @@ bool HelloWorld::init()
 
 void HelloWorld::update(float delta)
 {
-	//BOUNDARIES without using physics
-	bool canMove[4] = {true,true,true,true};
-	
-	if(!background->getBoundingBox().containsPoint(sprite1->getPosition()))
-	{
-		if (sprite1->getPositionX() <= background->getPositionX())
-		{
-			canMove[LEFT] = false;
-		}
-		if (sprite1->getPositionX() >= background->getPositionX() + 2 * background->getContentSize().width)
-		{
-			canMove[RIGHT] = false;
-		}
-		if (sprite1->getPositionY() <= background->getPositionY())
-		{
-			canMove[DOWN] = false;
-		}
-		if (sprite1->getPositionY() >= background->getPositionY() + 2 * background->getContentSize().height)
-		{
-			canMove[TOP] = false;
-		}
-	}
-
-	auto multiplyFactorDelta = 1 + delta;
-	if (arrow_states[LEFT] && canMove[LEFT])
-	{
-		sprite1->runAction(MoveBy::create(0, Vec2(multiplyFactorDelta * -10, 0)));
-	}
-	if (arrow_states[RIGHT] && canMove[RIGHT])
-	{
-		sprite1->runAction(MoveBy::create(0, Vec2(multiplyFactorDelta * 10, 0)));
-	}
-	if (arrow_states[TOP] && canMove[TOP])
-	{
-		sprite1->runAction(MoveBy::create(0, Vec2(0, multiplyFactorDelta * 10)));
-	}
-	if (arrow_states[DOWN] && canMove[DOWN])
-	{
-		sprite1->runAction(MoveBy::create(0, Vec2(0, multiplyFactorDelta * -10)));
-	}
-	
-	if(finishLineRectangle.containsPoint(sprite1->getPosition()))
-	{
-		MessageBox("Yay, you reached the finish line", "You win!");
-	}
+	std::unique_ptr<bool[]> canMove{ new bool[4]{ true,true,true,true } };
+	canMove = checkFinishLineReached(move(canMove));
+	canMove = checkBorders(move(canMove));
+	updateMovement(delta, move(canMove));
 }
 
 void HelloWorld::keyPressed(EventKeyboard::KeyCode keyCode, Event* event)
@@ -153,6 +126,74 @@ void HelloWorld::keyReleased(EventKeyboard::KeyCode keyCode, Event* event)
 		sprite1->stopAllActions();
 		sprite1->setSpriteFrame(animFrames.at(0));
 	}
+}
+
+std::unique_ptr<bool[]> HelloWorld::checkBorders(std::unique_ptr<bool[]> canMove) const
+{
+	if (!background->getBoundingBox().containsPoint(sprite1->getPosition()))
+	{
+		if (sprite1->getPositionX() <= background->getPositionX())
+		{
+			canMove[LEFT] = false;
+		}
+		if (sprite1->getPositionX() >= background->getPositionX() + 2 * background->getContentSize().width)
+		{
+			canMove[RIGHT] = false;
+		}
+		if (sprite1->getPositionY() <= background->getPositionY())
+		{
+			canMove[DOWN] = false;
+		}
+		if (sprite1->getPositionY() >= background->getPositionY() + 2 * background->getContentSize().height)
+		{
+			canMove[TOP] = false;
+		}
+	}
+	return move(canMove);
+}
+
+std::unique_ptr<bool[]> HelloWorld::checkFinishLineReached(std::unique_ptr<bool[]> pCanMove) const
+{
+	if (finishLineRectangle.containsPoint(sprite1->getPosition()))
+	{
+		if (sprite1->getChildren().size() < 1)
+		{
+			auto textField = ui::TextField::create("", "Arial", 300);
+			textField->setString("YOU WON!");
+			textField->setPosition(Vec2(100.0f, 550.0f));
+			sprite1->Node::addChild(textField);
+		}
+		sprite1->getPhysicsBody()->setLinearDamping(10.0f);
+		sprite1->getPhysicsBody()->resetForces();
+		pCanMove[LEFT] = false;
+		pCanMove[TOP] = false;
+		pCanMove[RIGHT] = false;
+		pCanMove[DOWN] = false;
+	}
+	return move(pCanMove);
+}
+
+std::unique_ptr<bool[]> HelloWorld::updateMovement(float delta, std::unique_ptr<bool[]> pCanMove) const
+{
+	auto multiplyFactorDelta = 1 + delta;
+	if (arrow_states[LEFT] && pCanMove[LEFT])
+	{
+		sprite1->runAction(MoveBy::create(0, Vec2(multiplyFactorDelta * -10, 0)));
+	}
+	if (arrow_states[RIGHT] && pCanMove[RIGHT])
+	{
+		sprite1->runAction(MoveBy::create(0, Vec2(multiplyFactorDelta * 10, 0)));
+	}
+	if (arrow_states[TOP] && pCanMove[TOP])
+	{
+		/*sprite1->runAction(MoveBy::create(0, Vec2(0, multiplyFactorDelta * 10)));*/
+		sprite1->getPhysicsBody()->applyForce(Vec2(0, multiplyFactorDelta * 200));
+	}
+	if (arrow_states[DOWN] && pCanMove[DOWN])
+	{
+		sprite1->runAction(MoveBy::create(0, Vec2(0, multiplyFactorDelta * -10)));
+	}
+	return move(pCanMove);
 }
 
 void HelloWorld::setPhysicsWorld(PhysicsWorld* world)
--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -5,24 +5,26 @@
 
 class HelloWorld : public cocos2d::Layer
 {
-public:
-	cocos2d::Sprite * sprite1;
-	cocos2d::experimental::TMXTiledMap * background;
+	cocos2d::PhysicsWorld* sceneWorld = nullptr;
+	cocos2d::Sprite * sprite1 = nullptr;
+	cocos2d::experimental::TMXTiledMap * background = nullptr;
 	cocos2d::Vector<cocos2d::SpriteFrame*> animFrames;
 	cocos2d::Rect finishLineRectangle;
-	enum ARROWS{LEFT, TOP, RIGHT, DOWN};
+	enum ARROWS { LEFT, TOP, RIGHT, DOWN };
 	bool arrow_states[4];
-
+public:
 	// there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-	void keyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event * event);
-	void keyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event * event);
 
 	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
-    virtual bool init();
+	bool init() override;
 	void update(float delta) override;
-private:
-	cocos2d::PhysicsWorld* sceneWorld;
+private:	
+	void keyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event * event);
+	void keyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event * event);
+	std::unique_ptr<bool[]> checkBorders(std::unique_ptr<bool[]> pCanMove) const;
+	std::unique_ptr<bool[]> checkFinishLineReached(std::unique_ptr<bool[]> pCanMove) const;
+	std::unique_ptr<bool[]> updateMovement(float pDelta, std::unique_ptr<bool[]> pCanMove) const;
 	void setPhysicsWorld(cocos2d::PhysicsWorld * world);
 
     // implement the "static create()" method manually
